72fb4dd52bda3ebdbaef0a262ff617880a799365
wheats <532770034@qq.com>
2017-04-18 22:11:46 +0800

    connect

--- connect/connect.js
+++ connect/connect.js
@@ -1,13 +1,20 @@
 'use strict';
 const http = require('http');
 const connect = require('connect');
+const url = require('url');
+const path = require('path');
 
-let app = connect();
+let app = connect(),
+    filePath = '';
 
 app.use((req, res, next)=>{
   console.log(req.url);
   next();
 });
 
+app.use((req, res)=>{
+
+  res.end('hello world!');
+});
 
 http.createServer(app).listen(8000);
